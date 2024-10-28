64fd5b2eec4b7941f4a5e6b5cf5a3fbb686d82df
Yoh5 <milohoue.aho@epitech.eu>
2024-10-28 12:02:03 +0100

    Add my portfolio

---
+++ src/constants.js
@@ -0,0 +1,68 @@
+import devops from "./assets/DevOps.avif"
+export const techStack = [
+
+  { name: "C Language", link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/c/c-plain.svg" },
+  { name: "C++", link: "https://raw.githubusercontent.com/devicons/devicon/master/icons/cplusplus/cplusplus-original.svg" },
+  { name: "C#", link: "https://raw.githubusercontent.com/devicons/devicon/master/icons/csharp/csharp-original.svg" },
+  { name: "HTML", link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" },
+  { name: "CSS", link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-plain.svg" },
+  { name: "Javascript", link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" },
+  { name: "NodeJS", link: "https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg" },
+  { name: "React", link: "https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg" },
+  { name: "React Native", link: "https://pagepro.co/blog/wp-content/uploads/2020/03/react-native-logo-884x1024.png" },
+  { name: "Git", link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-plain.svg" },
+  { name: "Flutter", link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/flutter/flutter-plain.svg" },
+  { name: "Python", link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg" },
+  { name: "Docker", link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original.svg" },
+  { name: "Kubernetes", link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/kubernetes/kubernetes-original.svg" },
+  { name: "Bash", link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bash/bash-original.svg" },
+  // { name: "Sass", link: "https://raw.githubusercontent.com/devicons/devicon/master/icons/sass/sass-original.svg" },
+
+//{ name: "Tailwind CSS", link: "https://www.vectorlogo.zone/logos/tailwindcss/tailwindcss-icon.svg" },
+  // { name: "MySQl", link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-plain.svg" },
+  // { name: "Mongo DB", link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-plain.svg" },
+  // { name: "Heroku", link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/heroku/heroku-plain.svg" },
+  // { name: "Azure", link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/azure/azure-plain.svg" },
+  // { name: "Docker", link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-plain.svg" },
+  // { name: "Firebase", link: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg" },
+
+  // { name: "Bash", link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bash/bash-original.svg" },
+  // { name: "Vim", link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vim/vim-original.svg" },
+
+  // { name: "VS Code", link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original.svg" },
+  // {name: "", link: "https://repository-images.githubusercontent.com/59065830/b62be480-45d2-11ea-9989-803db0f9c44d"}
+]
+
+
+export const contactLinks = [
+{ name: 'Instagram', url: 'https://img.icons8.com/doodle/40/000000/instagram-new--v2.png', link: 'https://www.instagram.com/axl_sam6/profilecard/?igsh=dndhY2t6Z2ZvN2R0' },
+{ name: 'Github', url: 'https://img.icons8.com/doodle/40/000000/github--v1.png', link :'https://github.com/Yoh5'},
+{ name: 'LinkedIn', url: 'https://img.icons8.com/doodle/40/000000/linkedin--v2.png', link: 'https://www.linkedin.com/in/axel-aho-30b01b235?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app'}
+]
+
+
+
+export const serviceData = [
+  {
+    name: 'Software Development',
+    desc: 'With my skills in C, C++, and Python, I offer services in developing basic system-level programs, optimizing code, creating command-line applications, automating tasks with Python scripts, and handling data processing or web scraping. I can also contribute to simple game projects and provide code refactoring or debugging services.',
+    img: "https://img.icons8.com/external-kiranshastry-lineal-color-kiranshastry/64/000000/external-web-development-coding-kiranshastry-lineal-color-kiranshastry.png"
+  },
+  {
+    name: 'Mobile Application Development',
+    desc: `With my developing skills in React Native and Flutter, I can assist in creating cross-platform mobile applications for Android and iOS. I can help design user-friendly interfaces, implement basic app functionalities, and integrate APIs. My goal is to deliver responsive mobile apps that provide a good user experience while I continue to improve my expertise in this area`,
+    img: "https://img.icons8.com/external-justicon-lineal-color-justicon/64/000000/external-app-development-responsive-web-design-justicon-lineal-color-justicon.png"
+  },
+
+  {
+    name: 'Backend Development',
+    desc: `With my developing skills in Back-End Development, I can assist in creating basic server-side applications using Node.js, develop RESTful APIs, and manage database interactions. I can also help with implementing user authentication and data storage solutions while focusing on building scalable and efficient back-end systems as I continue to enhance my expertise in this area`,
+    img: "https://img.icons8.com/external-flaticons-flat-flat-icons/64/000000/external-backend-no-code-flaticons-flat-flat-icons.png"
+  },
+
+  {
+    name: 'Containerization & Kubernetes management',
+    desc: `With my foundational skills in Kubernetes, Docker, and DevOps, I can assist with containerizing applications, setting up Kubernetes clusters, and streamlining basic deployment processes to improve operational efficiency.`,
+    img: devops
+  }
+]
---
+++ src/views/About.jsx
@@ -0,0 +1,89 @@
+import React, { useContext } from "react";
+import { techStack } from "../constants";
+import { ThemeContext } from "../themeProvider";
+import { motion } from "framer-motion";
+
+const About = () => {
+  const theme = useContext(ThemeContext);
+  const darkMode = theme.state.darkMode;
+  return (
+    <div id="about" className={darkMode === true ? "bg-white" : "bg-gray-900"}>
+      <div className="max-w-7xl mx-auto x-4 sm:px-6 lg:px-8 px-4 md:mt-0 pt-24 pb-12">
+        <h2
+          className={
+            darkMode
+              ? "text-5xl font-bold px-4 md:px-0 text-center"
+              : "text-5xl font-bold px-4 md:px-0 text-center text-white"
+          }
+        >
+          About Me
+        </h2>
+        <div>
+          <motion.div>
+            <h4 className="mt-12 text-3xl font-semibold text-blue-500">
+              A bit about me
+            </h4>
+            <p
+              className={
+                darkMode
+                  ? "mt-4 text-xl text-justify text-gray-500"
+                  : "mt-4 text-xl text-justify text-white"
+              }
+            >
+              I learned C/C++, DevOps and Mobile Development in school, 
+              but I’m a self-taught Back-End Developer with a 
+              focus on Python programming. I am continually honing 
+              my skills in Back-End Development and these programming 
+              languages. Two traits that best describe me are my passion 
+              for learning and exploring new things, as well as my ability 
+              to teach myself independently.
+            </p>
+          </motion.div>
+          <motion.div
+          >
+            <h4 className="mt-12 text-3xl font-semibold text-blue-500">
+              Technologies and Tools
+            </h4>
+            <p
+              className={
+                darkMode
+                  ? "mt-4 text-xl text-justify text-gray-500"
+                  : "mt-4 text-xl text-justify text-white"
+              }
+            >
+              Using modern technologies and open-source tools, 
+              I build user-friendly, efficient apps and websites 
+              for mobile and desktop. With skills in C, C++, Python, 
+              React, DevOps and Node.js, I create responsive, scalable solutions 
+              focused on great user experiences.
+            </p>
+          </motion.div>
+          <motion.div className="flex flex-wrap mt-8 flex flex-wrap justify-between ">
+            {techStack.map((el, index) => (
+              <motion.div
+                initial="hidden"
+                whileInView={"visible"}
+                variants={{
+                  visible: {
+                    y: 0,
+                    opacity: 1,
+                    transition: {
+                      type: "spring",
+                    },
+                  },
+                  hidden: { opacity: 1, y: 80 },
+                }}
+                className="py-2 px-4 bg-gray-50 md:m-4 mx-2 mt-6 rounded-lg flex items-center hover:scale-125 cursor-pointer md:w-48 w-40"
+              >
+                <img alt="" src={el.link} className="w-12" />
+                <h4 className="text-md ml-4">{el.name}</h4>
+              </motion.div>
+            ))}
+          </motion.div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
